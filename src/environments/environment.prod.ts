24a290eeffc4b5a509c13e4ca30df3069756404e
Punna00 <poko-nyan@hotmail.com>
2021-02-04 12:41:11 +0700

    fix bug Login Component Page

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDqOgCPp5xRhbR9jjTP-HmBpqaWVf2_HE",
+    authDomain: "chatbox-ea300.firebaseapp.com",
+    databaseURL: "https://chatbox-ea300-default-rtdb.firebaseio.com",
+    projectId: "chatbox-ea300",
+    storageBucket: "chatbox-ea300.appspot.com",
+    messagingSenderId: "490075001303",
+    appId: "1:490075001303:web:6c5c46bb1ad72449317b92"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCDqOgCPp5xRhbR9jjTP-HmBpqaWVf2_HE",
+    authDomain: "chatbox-ea300.firebaseapp.com",
+    databaseURL: "https://chatbox-ea300-default-rtdb.firebaseio.com",
+    projectId: "chatbox-ea300",
+    storageBucket: "chatbox-ea300.appspot.com",
+    messagingSenderId: "490075001303",
+    appId: "1:490075001303:web:6c5c46bb1ad72449317b92"
+  }
+};
